e765ce58bdd3a22b3dab731d5dee28cbbee6e8d6
eric.dalquist@gmail.com <eric.dalquist@gmail.com@e9fffd1a-4083-750a-f54f-a748c1f33b81>
2010-06-28 17:30:46 +0000

    [maven-release-plugin]  copy for tag ehcache-spring-annotations-1.1.0

---
+++ tags/ehcache-spring-annotations-1.1.0/src/main/java/com/googlecode/ehcache/annotations/key/HashCodeCacheKeyGenerator.java
@@ -0,0 +1,145 @@
+/**
+ * Copyright 2010 Nicholas Blair, Eric Dalquist
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.googlecode.ehcache.annotations.key;
+
+
+/**
+ * @author Eric Dalquist
+ * @version $Revision$
+ */
+public class HashCodeCacheKeyGenerator extends AbstractHashingCacheKeyGenerator<HashCodeCacheKeyGenerator.LongGenerator, Long> {
+    /**
+     * Name of the bean this generator is registered under using the default constructor.
+     */
+    public static final String DEFAULT_BEAN_NAME = "com.googlecode.ehcache.annotations.key.HashCodeCacheKeyGenerator.DEFAULT_BEAN_NAME";
+    
+    protected static final long INITIAL_HASH = 1;
+    protected static final long MULTIPLIER = 31;
+    
+    /**
+     * Little utility class to fake a mutable long
+     */
+    public static class LongGenerator {
+        private long hash = INITIAL_HASH;
+        
+        private LongGenerator() {
+        }
+    }
+    
+    /**
+     * @see AbstractCacheKeyGenerator#AbstractCacheKeyGenerator() 
+     */
+    public HashCodeCacheKeyGenerator() {
+    }
+
+    /**
+     * @see AbstractCacheKeyGenerator#AbstractCacheKeyGenerator(boolean, boolean) 
+     */
+    public HashCodeCacheKeyGenerator(boolean includeMethod, boolean includeParameterTypes) {
+        super(includeMethod, includeParameterTypes);
+    }
+
+    @Override
+    public LongGenerator getGenerator(Object... data) {
+        return new LongGenerator();
+    }
+
+    @Override
+    public Long generateKey(LongGenerator generator) {
+        return generator.hash;
+    }
+    
+
+    @Override
+    protected void append(LongGenerator generator, boolean[] a) {
+        for (final boolean element : a) {
+            generator.hash = MULTIPLIER * generator.hash + (element ? 1231 : 1237);
+        }
+    }
+
+    @Override
+    protected void append(LongGenerator generator, byte[] a) {
+        for (final byte element : a) {
+            generator.hash = MULTIPLIER * generator.hash + element;
+        }
+    }
+
+    @Override
+    protected void append(LongGenerator generator, char[] a) {
+        for (final char element : a) {
+            generator.hash = MULTIPLIER * generator.hash + element;
+        }
+    }
+
+    @Override
+    protected void append(LongGenerator generator, double[] a) {
+        for (final double element : a) {
+            generator.hash = MULTIPLIER * generator.hash + Double.doubleToLongBits(element);
+        }
+    }
+
+    @Override
+    protected void append(LongGenerator generator, float[] a) {
+        for (final float element : a) {
+            generator.hash = MULTIPLIER * generator.hash + Float.floatToIntBits(element);
+        }
+    }
+
+    @Override
+    protected void append(LongGenerator generator, int[] a) {
+        for (final int element : a) {
+            generator.hash = MULTIPLIER * generator.hash + element;
+        }
+    }
+
+    @Override
+    protected void append(LongGenerator generator, long[] a) {
+        for (final long element : a) {
+            generator.hash = MULTIPLIER * generator.hash + element;
+        }
+    }
+
+    @Override
+    protected void append(LongGenerator generator, short[] a) {
+        for (final short element : a) {
+            generator.hash = MULTIPLIER * generator.hash + element;
+        }
+    }
+
+    @Override
+    protected void appendGraphCycle(LongGenerator generator, Object o) {
+        generator.hash = MULTIPLIER * generator.hash;
+    }
+
+    @Override
+    protected void appendNull(LongGenerator generator) {
+        generator.hash = MULTIPLIER * generator.hash;
+    }
+    
+    @Override
+    protected void appendHash(LongGenerator generator, Object e) {
+        if (e instanceof Double) {
+            generator.hash = MULTIPLIER * generator.hash + Double.doubleToLongBits(((Double)e).doubleValue());
+        }
+        else if (e instanceof Long) {
+            generator.hash = MULTIPLIER * generator.hash + ((Long)e).longValue();
+        }
+        else {        
+            generator.hash = MULTIPLIER * generator.hash + e.hashCode();
+        }
+    }
+}
---
+++ tags/ehcache-spring-annotations-1.1.0/src/main/java/com/googlecode/ehcache/annotations/key/MessageDigestCacheKeyGenerator.java
@@ -0,0 +1,223 @@
+/**
+ * Copyright 2010 Nicholas Blair, Eric Dalquist
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.googlecode.ehcache.annotations.key;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import org.apache.commons.codec.binary.Base64;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Eric Dalquist
+ * @version $Revision$
+ */
+public class MessageDigestCacheKeyGenerator extends AbstractHashingCacheKeyGenerator<MessageDigestOutputStream, String> {
+    /**
+     * Name of the bean this generator is registered under using the default constructor.
+     */
+    public static final String DEFAULT_BEAN_NAME = "com.googlecode.ehcache.annotations.key.MessageDigestCacheKeyGenerator.DEFAULT_BEAN_NAME";
+    
+    public static final String DEFAULT_ALGORITHM = "SHA-1";
+    protected static final int DEFAULT_BYTE_BUFFER_SIZE = 64;
+    
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+    
+    private final MessageDigest baseMessageDigest;
+    private boolean cloneNotSupported;
+    
+    /**
+     * Uses {@link #DEFAULT_ALGORITHM} for the algorithm
+     * @see AbstractCacheKeyGenerator#AbstractCacheKeyGenerator() 
+     */
+    public MessageDigestCacheKeyGenerator() throws NoSuchAlgorithmException {
+        this(DEFAULT_ALGORITHM);
+    }
+    
+    /**
+     * @see AbstractCacheKeyGenerator#AbstractCacheKeyGenerator() 
+     */
+    public MessageDigestCacheKeyGenerator(String algorithm) throws NoSuchAlgorithmException {
+        this.baseMessageDigest = MessageDigest.getInstance(algorithm);
+    }
+    
+    /**
+     * Uses {@link #DEFAULT_ALGORITHM} for the algorithm
+     * @see AbstractCacheKeyGenerator#AbstractCacheKeyGenerator(boolean, boolean) 
+     */
+    public MessageDigestCacheKeyGenerator(boolean includeMethod, boolean includeParameterTypes) throws NoSuchAlgorithmException {
+        this(DEFAULT_ALGORITHM, includeMethod, includeParameterTypes);
+    }
+    
+    /**
+     * @see AbstractCacheKeyGenerator#AbstractCacheKeyGenerator(boolean, boolean) 
+     */
+    public MessageDigestCacheKeyGenerator(String algorithm, boolean includeMethod, boolean includeParameterTypes) throws NoSuchAlgorithmException {
+        super(includeMethod, includeParameterTypes);
+        this.baseMessageDigest = MessageDigest.getInstance(algorithm);
+    }
+    
+
+
+    @Override
+    public MessageDigestOutputStream getGenerator(Object... data) {
+        final MessageDigest messageDigest = this.getMessageDigest();
+        return new MessageDigestOutputStream(messageDigest);
+    }
+
+    @Override
+    public String generateKey(MessageDigestOutputStream generator) {
+        final MessageDigest messageDigest = generator.getMessageDigest();
+        final byte[] digest = messageDigest.digest();
+        return this.encodeHash(digest);
+    }
+
+    /**
+     * Encode the digested hash bytes as a String
+     */
+    protected String encodeHash(final byte[] digest) {
+        return Base64.encodeBase64URLSafeString(digest);
+    }
+
+    /**
+     * Tries to clone the {@link MessageDigest} that was created during construction. If the clone fails
+     * that is remembered and from that point on new {@link MessageDigest} instances will be created on
+     * every call.
+     * 
+     * @return Generates a {@link MessageDigest} to use during a call to {@link #generateKey(Object...)}
+     */
+    protected MessageDigest getMessageDigest() {
+        if (this.cloneNotSupported) {
+            final String algorithm = this.baseMessageDigest.getAlgorithm();
+            try {
+                return MessageDigest.getInstance(algorithm);
+            }
+            catch (NoSuchAlgorithmException e) {
+                throw new IllegalStateException("MessageDigest algorithm '" + algorithm + "' was supported when " + this.getClass().getSimpleName() + " was created but is not now. This should not be possible.", e);
+            }
+        }
+        
+        try {
+            return (MessageDigest)this.baseMessageDigest.clone();
+        }
+        catch (CloneNotSupportedException e) {
+            this.cloneNotSupported = true;
+            this.logger.warn("Could not clone MessageDigest using algorithm '" + this.baseMessageDigest.getAlgorithm() + "'. MessageDigest.getInstance will be used from now on which will be much more expensive.", e);
+            return this.getMessageDigest();
+        }
+    }
+    
+
+    @Override
+    protected void append(MessageDigestOutputStream generator, boolean[] a) {
+        for (final boolean element : a) {
+            generator.writeBoolean(element);
+        }
+    }
+
+    @Override
+    protected void append(MessageDigestOutputStream generator, byte[] a) {
+        generator.write(a);
+    }
+
+    @Override
+    protected void append(MessageDigestOutputStream generator, char[] a) {
+        for (final char element : a) {
+            generator.writeChar(element);
+        }
+    }
+
+    @Override
+    protected void append(MessageDigestOutputStream generator, double[] a) {
+        for (final double element : a) {
+            generator.writeDouble(element);
+        }
+    }
+
+    @Override
+    protected void append(MessageDigestOutputStream generator, float[] a) {
+        for (final float element : a) {
+            generator.writeFloat(element);
+        }
+    }
+
+    @Override
+    protected void append(MessageDigestOutputStream generator, int[] a) {
+        for (final int element : a) {
+            generator.writeInt(element);
+        }
+    }
+
+    @Override
+    protected void append(MessageDigestOutputStream generator, long[] a) {
+        for (final long element : a) {
+            generator.writeLong(element);
+        }
+    }
+
+    @Override
+    protected void append(MessageDigestOutputStream generator, short[] a) {
+        for (final short element : a) {
+            generator.writeShort(element);
+        }
+    }
+
+    @Override
+    protected void appendGraphCycle(MessageDigestOutputStream generator, Object o) {
+        generator.write(0);        
+    }
+
+    @Override
+    protected void appendNull(MessageDigestOutputStream generator) {
+        generator.write(0);
+    }
+
+    @Override
+    protected void appendHash(MessageDigestOutputStream generator, Object e) {
+        if (e instanceof String) {
+            generator.writeUTF((String)e);
+        }
+        else if (e instanceof Boolean) {
+            generator.writeBoolean((Boolean)e);
+        }
+        else if (e instanceof Byte) {
+            generator.writeByte((Byte)e);
+        }
+        else if (e instanceof Character) {
+            generator.writeChar((Character)e);
+        }
+        else if (e instanceof Double) {
+            generator.writeDouble((Double)e);
+        }
+        else if (e instanceof Float) {
+            generator.writeFloat((Float)e);
+        }
+        else if (e instanceof Integer) {
+            generator.writeInt((Integer)e);
+        }
+        else if (e instanceof Long) {
+            generator.writeLong((Long)e);
+        }
+        else if (e instanceof Short) {
+            generator.writeShort((Short)e);
+        }
+        else {        
+            generator.writeInt(e.hashCode());
+        }
+    }
+}
